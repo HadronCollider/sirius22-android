733117431d8b8ffab1011777c1094384a510e8d2
DESKTOP-IVVQ3M0\Ilya <ilya.matveev007@gmail.com>
2022-12-18 21:52:02 +0300

    Изменён стиль описания классов.

--- app/src/main/java/com/example/siriusproject/MainActivity.kt
+++ app/src/main/java/com/example/siriusproject/MainActivity.kt
@@ -3,8 +3,7 @@ package com.example.siriusproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
+
 import android.widget.Button
 import androidx.appcompat.app.ActionBar
 
@@ -14,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar?.setCustomView(R.layout.toolbar_main)
-        val newProjectActivity = Intent(this, New_project::class.java)
+        val newProjectActivity = Intent(this, NewProject::class.java)
         val newProjectButton = findViewById<Button>(R.id.new_project)
         newProjectButton.setOnClickListener {
             startActivity(newProjectActivity)
--- app/src/main/java/com/example/siriusproject/project.kt
+++ app/src/main/java/com/example/siriusproject/project.kt
@@ -3,7 +3,7 @@ package com.example.siriusproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class project : AppCompatActivity() {
+class Project : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project)
