64e167b1561448993275f9152fcf9dc9823a2a03
DESKTOP-IVVQ3M0\Ilya <ilya.matveev007@gmail.com>
2023-01-02 00:26:01 +0300

    Сделано нормальное расположение кнопки + на 1-ом экране, сдлано место для вывода названия проекта на экране проекта.

--- app/src/main/java/com/example/siriusproject/project.kt
+++ app/src/main/java/com/example/siriusproject/project.kt
@@ -2,10 +2,13 @@ package com.example.siriusproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class project : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project)
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setCustomView(R.layout.toolbar_activity_project)
     }
 }
