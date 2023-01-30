f03ed5c4744f3436a9f636f4300915a16ae342a4
DESKTOP-IVVQ3M0\Ilya <ilya.matveev007@gmail.com>
2023-01-30 18:35:07 +0300

    Убраны вкладки, на экране проекта теперь показываются фотографии проекта. Добавлена Activity для просмотра модели.

---
+++ app/src/main/java/com/example/siriusproject/ModelPreview.kt
@@ -0,0 +1,11 @@
+package com.example.siriusproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModelPreview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_model_preview)
+    }
+}
--- app/src/main/java/com/example/siriusproject/ProjectActivity.kt
+++ app/src/main/java/com/example/siriusproject/ProjectActivity.kt
@@ -4,20 +4,9 @@ package com.example.siriusproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 
 class ProjectActivity : AppCompatActivity() {
 
-    private lateinit var adapter: FragmentAdapter
-    private lateinit var viewPager: ViewPager2
-    private lateinit var tabLayout: TabLayout
-
-    private val tabNames: Array<String> = arrayOf(
-        "модель",
-        "фотографии"
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,24 +14,5 @@ class ProjectActivity : AppCompatActivity() {
         supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar?.setCustomView(R.layout.toolbar_activity_project)
 
-        adapter = FragmentAdapter(this)
-        viewPager = findViewById(R.id.tabs)
-        viewPager.adapter = adapter
-        val ft = supportFragmentManager.beginTransaction()
-
-        tabLayout = findViewById(R.id.tab_layout)
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
-            tab.text = tabNames[position]
-            when (position) {
-                0 -> {
-                    val fragment = ModelFragment()
-                    ft.replace(R.id.tabs, fragment)
-                }
-                else -> {
-                    val fragment = ImagesFragment()
-                    ft.replace(R.id.tabs, fragment)
-                }
-            }
-        }.attach()
     }
 }
