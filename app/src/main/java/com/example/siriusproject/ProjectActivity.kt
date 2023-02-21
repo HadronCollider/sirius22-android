a46b7b80c16cdc48e987f14fa7824c65e1daea42
IlyaMatveev007 <70830711+IlyaMatveev007@users.noreply.github.com>
2023-02-21 21:48:44 +0300

    Добавлено считывание даты последнего редактирования, заполнение полей при создании нового проекта.

--- app/src/main/java/com/example/siriusproject/ProjectActivity.kt
+++ app/src/main/java/com/example/siriusproject/ProjectActivity.kt
@@ -4,8 +4,16 @@ package com.example.siriusproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.ActionBar
+import com.example.siriusproject.data.ReadProjectData
+import java.util.Calendar
+import java.util.Date
 
 class ProjectActivity : AppCompatActivity() {
+
+    private var id = 1
+    private var name = ""
+    private var quality: Byte = 0
+    private lateinit var date: Date
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +21,29 @@ class ProjectActivity : AppCompatActivity() {
         setContentView(R.layout.activity_project)
         supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar?.setCustomView(R.layout.toolbar_activity_project)
+        val arguments = intent.extras
+        val allData = ReadProjectData(this.filesDir)
+        if (arguments?.getBoolean(R.string.type_type.toString()) == false) {
+            name = arguments.getString(R.string.name_type.toString()).toString()
+            quality = arguments.getByte(R.string.quality_type.toString())
+            date = Calendar.getInstance().time
+            if (allData.allProjectsData.isNotEmpty()) {
+                id = allData.allProjectsData[(allData.allProjectsData.size - 1)].id + 1
+            }
+        }
+    }
 
+    override fun onPause() {
+        super.onPause()
+        date = Calendar.getInstance().time
+    }
+
+    override fun onStop() {
+        super.onStop()
+        date = Calendar.getInstance().time
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        date = Calendar.getInstance().time
     }
 }
