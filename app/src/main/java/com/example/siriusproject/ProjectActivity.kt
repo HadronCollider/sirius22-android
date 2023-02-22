dbae0f4e66ad9155159b49d81ba6ff4fe72e4a95
IlyaMatveev007 <70830711+IlyaMatveev007@users.noreply.github.com>
2023-02-22 16:09:11 +0300

    Сделан выбор изображения.

--- app/src/main/java/com/example/siriusproject/ProjectActivity.kt
+++ app/src/main/java/com/example/siriusproject/ProjectActivity.kt
@@ -1,18 +1,51 @@
 package com.example.siriusproject
 
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.ActionBar
+import java.io.IOException
 
 class ProjectActivity : AppCompatActivity() {
 
+    private var galleryRequest = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_project)
         supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar?.setCustomView(R.layout.toolbar_activity_project)
+        val addImage = findViewById<Button>(R.id.add_images)
+        addImage.setOnClickListener {
+            val photoPickerIntent = Intent(Intent.ACTION_PICK)
+            photoPickerIntent.type = "image/*"
+            startActivityForResult(photoPickerIntent, galleryRequest)
+        }
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        var bitmap: Bitmap? = null
+        var imageView:ImageView = findViewById(R.id.imagePreview)
+        when(requestCode) {
+            galleryRequest -> {
+                if (resultCode == RESULT_OK) {
+                    var selectedImage: Uri? = data?.data
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedImage)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                    imageView.setImageBitmap(bitmap)
+                }
+            }
+        }
     }
 }
